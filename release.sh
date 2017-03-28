6fc3f71da01fdf4d504d5a3e660f5c5b48570f9e
David Lechner <david@lechnology.com>
2017-03-28 16:23:59 -0500

    Target stretch

--- release.sh
+++ release.sh
@@ -6,9 +6,11 @@ set -e
 
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
+distribution=$(dpkg-parsechangelog -S Distribution)
+codename=$(debian-distro-info --codename --${distribution})
 
-OS=raspbian DIST=jessie ARCH=armhf pbuilder-ev3dev build
-debsign ~/pbuilder-ev3dev/raspbian/jessie-armhf/${source}_${version}_armhf.changes
-dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/jessie-armhf/${source}_${version}_armhf.changes
+OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
+debsign ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
+dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
 
 gbp buildpackage --git-tag-only
